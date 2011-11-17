83b53a2ff4ef8224e805d4ab32bdb857c5c2236c
Sathya Sekaran <sfsekaran@gmail.com>
2011-11-16 22:24:11 -0800

    Changing the project creation api to .create -- .new is for class operations

--- lib/awesm.rb
+++ lib/awesm.rb
@@ -6,12 +6,15 @@ require 'json'
 module Awesm
   class Project
     include HTTParty
-    base_uri "http://api.awe.sm/projects"
+    base_uri 'http://api.awe.sm/projects'
 
-    def initialize(attributes)
-      self.class.post("/new", :query => { :subscription_key => attributes[:subscription_key], :json => { :name => attributes[:name] }.to_json })
+    def initialize(attributes = {})
+      # self.attributes = attributes
+    end
+
+    def self.create(attributes)
+      response = post('/new', :query => { :subscription_key => attributes[:subscription_key], :json => { :name => attributes[:name] }.to_json })
+      Awesm::Project.new(response['response']['project'])
     end
   end
 end
-
-
--- spec/awesm_spec.rb
+++ spec/awesm_spec.rb
@@ -1,19 +1,44 @@
 require 'spec_helper'
 
 describe Awesm::Project do
-  context '.new' do
+  context '.create' do
+    let(:json_response) do
+      %|
+        {
+          "request": {
+              "json": "{\"name\":\"myNewProject\"}",
+              "method": "new",
+              "object": "project",
+              "subscription_key": "sub-xXXxXxxX"
+          },
+          "response": {
+              "project": {
+                  "admins": [],
+                  "api_key": "6xxxxxxxxx58xx0xxx74xx3x76xx83x6x34xx48x7xxxx55x167037818d65x66x",
+                  "created_at": "2011-10-25 00:43:49",
+                  "default_domain": "awe.sm",
+                  "domains": [],
+                  "name": "myNewProject",
+                  "sharers": [],
+                  "updated_at": "2011-10-25 00:43:49",
+                  "viewers": []
+              }
+          }
+      }
+    |
+  end
 
     before do
       stub_request(:post, "http://api.awe.sm/projects/new?json=%7B%22name%22:%22Totally%20Awesome%20Project%22%7D&subscription_key=sub-xxxxxx").
-         to_return(:status => 200, :body => "", :headers => {})
+         to_return(:status => 200, :body => json_response, :headers => {})
     end
 
     it 'creates a project' do
-      Awesm::Project.new(:subscription_key => "sub-xxxxxx", :name => "Totally Awesome Project").should be_an_instance_of(Awesm::Project)
+      Awesm::Project.create(:subscription_key => "sub-xxxxxx", :name => "Totally Awesome Project").should be_an_instance_of(Awesm::Project)
     end
 
     it 'posts to the awe.sm project creation api properly' do
-      Awesm::Project.new({ :subscription_key => "sub-xxxxxx", :name => "Totally Awesome Project" })
+      Awesm::Project.create({ :subscription_key => "sub-xxxxxx", :name => "Totally Awesome Project" })
       a_request(:post, "http://api.awe.sm/projects/new").with(:query => {:subscription_key => "sub-xxxxxx", :json => { "name" => "Totally Awesome Project" }.to_json }).should have_been_made.once
     end
   end
