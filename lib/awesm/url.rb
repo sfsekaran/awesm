295bd7f1d67f60587ff507460fc37e085e963e38
Sathya Sekaran <sfsekaran@gmail.com>
2011-12-05 15:30:59 -0800

    Minor refactor of method name (required_params_present_for_share?)

--- lib/awesm/url.rb
+++ lib/awesm/url.rb
@@ -1,12 +1,12 @@
 module Awesm
   class Url
     def self.share(params)
-      if share_params_present?(params)
+      if required_params_present_for_share?(params)
         "http://api.awe.sm/url/share?v=3&url=#{params[:url]}&key=#{params[:key]}&tool=#{params[:tool]}&channel=#{params[:channel]}&destination=#{params[:destination]}"
       end
     end
 
-    def self.share_params_present?(params)
+    def self.required_params_present_for_share?(params)
       [:url, :key, :tool, :channel, :destination].all? do |param|
         params.include?(param)
       end
