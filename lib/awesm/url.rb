9eda55fb37d974a497fda98af234ab3813cd57f1
Sathya Sekaran <sfsekaran@gmail.com>
2011-12-08 02:51:55 -0800

    Allowing Awesm::Url.share to accept optional params.

--- lib/awesm/url.rb
+++ lib/awesm/url.rb
@@ -1,13 +1,20 @@
 module Awesm
   class Url
-    def self.share(params)
+    REQUIRED_SHARE_PARAMS = [:url, :key, :tool, :channel, :destination].freeze
+
+    def self.share(params = {})
       if required_params_present_for_share?(params)
-        "http://api.awe.sm/url/share?v=3&url=#{params[:url]}&key=#{params[:key]}&tool=#{params[:tool]}&channel=#{params[:channel]}&destination=#{params[:destination]}"
+        options = params.clone
+        options = options.delete_if{|key,value| REQUIRED_SHARE_PARAMS.include?(key) }
+        query = options.map{|k,v| "#{k}=#{v}"}.join('&')
+        share_url = "http://api.awe.sm/url/share?v=3&url=#{params[:url]}&key=#{params[:key]}&tool=#{params[:tool]}&channel=#{params[:channel]}&destination=#{params[:destination]}"
+        share_url += "&#{query}" if query.length > 0
+        share_url
       end
     end
 
     def self.required_params_present_for_share?(params)
-      [:url, :key, :tool, :channel, :destination].all? do |param|
+      REQUIRED_SHARE_PARAMS.all? do |param|
         params.include?(param)
       end
     end
--- spec/awesm/url_spec.rb
+++ spec/awesm/url_spec.rb
@@ -2,7 +2,17 @@ require 'spec_helper'
 
 describe Awesm::Url do
   describe '.share' do
-    it 'returns the correct awe.sm url' do
+    let(:required_parameters) do
+      {
+        :url => 'http://developers.awe.sm/',
+        :key => '5c8b1a212434c2153c2f2c2f2c765a36140add243bf6eae876345f8fd11045d9',
+        :tool => 'mKU7uN',
+        :channel => 'twitter',
+        :destination => 'http://twitter.com/intent/tweet?text=This+is+the+coolest+API+evar!%26url=AWESM_URL%26via=awesm'
+      }
+    end
+
+    it 'returns the correct awe.sm url when passed the required parameters' do
       Awesm::Url.share(:url => 'http://developers.awe.sm/',
                        :key => '5c8b1a212434c2153c2f2c2f2c765a36140add243bf6eae876345f8fd11045d9',
                        :tool => 'mKU7uN',
@@ -11,9 +21,28 @@ describe Awesm::Url do
         should == 'http://api.awe.sm/url/share?v=3&url=http://developers.awe.sm/&key=5c8b1a212434c2153c2f2c2f2c765a36140add243bf6eae876345f8fd11045d9&tool=mKU7uN&channel=twitter&destination=http://twitter.com/intent/tweet?text=This+is+the+coolest+API+evar!%26url=AWESM_URL%26via=awesm'
     end
 
-    it 'returns nil if no options are sent in' do
+    it 'returns nil if it receives an empty hash' do
       Awesm::Url.share({}).should == nil
     end
+
+    it 'returns nil if it receives nothing' do
+      Awesm::Url.share.should == nil
+    end
+
+    [[:campaign,             'my-campaign-1'],
+     [:campaign_description, 'This is a description. Fun.'],
+     [:campaign_name,        'My Campaign 1'],
+     [:notes,                'This link is going to take down my site.'],
+     [:parent,               'awe.sm_s5d99'],
+     [:service_userid,       'twitter:13263'],
+     [:tag,                  'My-Tag'],
+     [:user_id,              '42']].each do |option, value|
+      it "returns the correct awe.sm url when also passed a '#{option.to_s}' parameter" do
+        Awesm::Url.share(required_parameters.merge(option => value)).
+          should == "http://api.awe.sm/url/share?v=3&url=http://developers.awe.sm/&key=5c8b1a212434c2153c2f2c2f2c765a36140add243bf6eae876345f8fd11045d9&tool=mKU7uN&channel=twitter&destination=http://twitter.com/intent/tweet?text=This+is+the+coolest+API+evar!%26url=AWESM_URL%26via=awesm&#{option.to_s}=#{value}"
+      end
+    end
+
   end
 end
 
