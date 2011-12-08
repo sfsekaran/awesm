379f6cd6931990c7531d68d28fbc3c7b267e8549
Sathya Sekaran <sfsekaran@gmail.com>
2011-12-08 12:51:12 -0800

    Make sure Awesm::Url.share accepts the user_id related params.

--- spec/awesm/url_spec.rb
+++ spec/awesm/url_spec.rb
@@ -43,6 +43,16 @@ describe Awesm::Url do
       end
     end
 
+    it "accepts user_id related parameters" do
+      url = Awesm::Url.share(required_parameters.merge(
+        :user_id             => 42,
+        :user_id_icon_url    => 'http://test.com/users/42/avatar.png',
+        :user_id_profile_url => 'http://test.com/users/42/',
+        :user_id_username    => 'johndoe@test.com'
+      ))
+      url.should == "http://api.awe.sm/url/share?v=3&url=http://developers.awe.sm/&key=5c8b1a212434c2153c2f2c2f2c765a36140add243bf6eae876345f8fd11045d9&tool=mKU7uN&channel=twitter&destination=http://twitter.com/intent/tweet?text=This+is+the+coolest+API+evar!%26url=AWESM_URL%26via=awesm&user_id_icon_url=http://test.com/users/42/avatar.png&user_id_profile_url=http://test.com/users/42/&user_id_username=johndoe@test.com&user_id=42"
+    end
+
   end
 end
 
