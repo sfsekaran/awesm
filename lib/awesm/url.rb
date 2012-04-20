5ee9657610e2c893d447a565ba9b43549f86c2be
Mike Durnhofer <mdurnhofer@gmail.com>
2012-04-20 15:43:52 -0700

    Awesm::Url.static added

--- lib/awesm/url.rb
+++ lib/awesm/url.rb
@@ -1,9 +1,10 @@
 module Awesm
   class Url
     REQUIRED_SHARE_PARAMS = [:url, :key, :tool, :channel, :destination].freeze
+    REQUIRED_STATIC_PARAMS = [:format, :url, :key, :tool].freeze
 
     def self.share(params = {})
-      if required_params_present_for_share?(params)
+      if required_params_present?(REQUIRED_SHARE_PARAMS, params)
         options = params.clone
         options = options.delete_if{|key,value| REQUIRED_SHARE_PARAMS.include?(key) }
         query = options.map{|k,v| "#{k}=#{v}"}.join('&')
@@ -13,8 +14,19 @@ module Awesm
       end
     end
 
-    def self.required_params_present_for_share?(params)
-      REQUIRED_SHARE_PARAMS.all? do |param|
+    def self.static(params = {})
+      if required_params_present?(REQUIRED_STATIC_PARAMS, params)
+        options = params.clone
+        options = options.delete_if{|key,value| REQUIRED_STATIC_PARAMS.include?(key) }
+        query = options.map{|k,v| "#{k}=#{v}"}.join('&')
+        static_url = "http://api.awe.sm/url/static.#{params[:format]}?v=3&url=#{params[:url]}&key=#{params[:key]}&tool=#{params[:tool]}"
+        static_url += "&#{query}" if query.length > 0
+        static_url
+      end
+    end
+
+    def self.required_params_present?(required_params, params)
+      required_params.all? do |param|
         params.include?(param)
       end
     end
--- spec/awesm/url_spec.rb
+++ spec/awesm/url_spec.rb
@@ -54,5 +54,58 @@ describe Awesm::Url do
     end
 
   end
+
+  describe '.static' do
+    let(:required_parameters) do
+      {
+        :format => 'json',
+        :url => 'http://developers.awe.sm/',
+        :key => '5c8b1a212434c2153c2f2c2f2c765a36140add243bf6eae876345f8fd11045d9',
+        :tool => 'mKU7uN'
+      }
+    end
+
+    it 'returns the correct static awe.sm url when passed the required parameters' do
+      Awesm::Url.static(:format => 'json',
+                       :url => 'http://developers.awe.sm/',
+                       :key => '5c8b1a212434c2153c2f2c2f2c765a36140add243bf6eae876345f8fd11045d9',
+                       :tool => 'mKU7uN').
+        should == 'http://api.awe.sm/url/static.json?v=3&url=http://developers.awe.sm/&key=5c8b1a212434c2153c2f2c2f2c765a36140add243bf6eae876345f8fd11045d9&tool=mKU7uN'
+    end
+
+    it 'returns nil if it receives an empty hash' do
+      Awesm::Url.static({}).should == nil
+    end
+
+    it 'returns nil if it receives nothing' do
+      Awesm::Url.static.should == nil
+    end
+
+    [[:channel,              'twitter'],
+     [:campaign,             'my-campaign-1'],
+     [:campaign_description, 'This is a description. Fun.'],
+     [:campaign_name,        'My Campaign 1'],
+     [:notes,                'This link is going to take down my site.'],
+     [:parent,               'awe.sm_s5d99'],
+     [:service_userid,       'twitter:13263'],
+     [:tag,                  'My-Tag'],
+     [:user_id,              '42']].each do |option, value|
+      it "returns the correct static awe.sm url when also passed a '#{option.to_s}' parameter" do
+        Awesm::Url.static(required_parameters.merge(option => value)).
+          should == "http://api.awe.sm/url/static.json?v=3&url=http://developers.awe.sm/&key=5c8b1a212434c2153c2f2c2f2c765a36140add243bf6eae876345f8fd11045d9&tool=mKU7uN&#{option.to_s}=#{value}"
+      end
+    end
+
+    it "accepts user_id related parameters" do
+      url = Awesm::Url.static(required_parameters.merge(
+        :user_id             => 42,
+        :user_id_icon_url    => 'http://test.com/users/42/avatar.png',
+        :user_id_profile_url => 'http://test.com/users/42/',
+        :user_id_username    => 'johndoe@test.com'
+      ))
+      url.should == "http://api.awe.sm/url/static.json?v=3&url=http://developers.awe.sm/&key=5c8b1a212434c2153c2f2c2f2c765a36140add243bf6eae876345f8fd11045d9&tool=mKU7uN&user_id=42&user_id_icon_url=http://test.com/users/42/avatar.png&user_id_profile_url=http://test.com/users/42/&user_id_username=johndoe@test.com"
+    end
+
+  end
 end
 
